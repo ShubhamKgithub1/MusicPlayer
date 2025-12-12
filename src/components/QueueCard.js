96d425201afeee12a7218e260e8ca2100ef7bb9c
Shubham <sk715152@gmail.com>
2025-12-12 14:26:52 +0530

    QueueCards modified with memoized props

--- src/components/QueueCard.js
+++ src/components/QueueCard.js
@@ -1,31 +1,32 @@
-import { useDispatch } from "react-redux";
-import { setCurrentSongIndex, setCurrentSong,playPause } from "../reduxStore/playerSlice";
 import { useSelector } from "react-redux";
 import React from "react";
 
-const QueueCard = ({track})=>{
-    const dispatch = useDispatch();
-    const queue = useSelector((state)=>state.player.queue);
+const QueueCard = ({ track, onSelect }) => {
+  const queue = useSelector((state) => state.player.queue);
 
-const handleTrackClick =(trackId) => {
-    const index = queue.findIndex((track)=>track.id === trackId);
-    if (index !== -1) {
-        dispatch(setCurrentSongIndex(index));
-        dispatch(setCurrentSong(queue[index]));
-        dispatch(playPause(true));        
-    }
-};
-    if(!queue) return null;
-    return (
-        <div className="relative group flex items-center rounded-lg gap-3 p-2 cursor-pointer" onClick={()=>handleTrackClick(track?.id)}>
-            <div className="absolute inset-0 bg-gradient-to-r from-transparent via-white/30 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none"></div>
-            <img src={track?.album?.cover_small} alt={track?.title} className="w-12 h-12 rounded-full"/>
-            <div className="flex flex-col truncate relative z-20">
-                <span className="text-sm font-bold dark:text-white truncate">{track?.title_short}</span>
-                <span className="text-xs font-semibold dark:text-gray-300 truncate">{track?.artist?.name}</span>
-            </div>
-        </div>
-    );
+
+  if (!queue) return null;
+  return (
+    <div
+      className="relative group flex items-center rounded-lg gap-3 p-2 cursor-pointer"
+      onClick={() => onSelect(track?.id)}
+    >
+      <div className="absolute inset-0 bg-gradient-to-r from-transparent via-white/30 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none"></div>
+      <img
+        src={track?.album?.cover_small}
+        alt={track?.title}
+        className="w-12 h-12 rounded-full"
+      />
+      <div className="flex flex-col truncate relative z-20">
+        <span className="text-sm font-bold dark:text-white truncate">
+          {track?.title_short}
+        </span>
+        <span className="text-xs font-semibold dark:text-gray-300 truncate">
+          {track?.artist?.name}
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default React.memo(QueueCard);
--- src/components/playbar/PlaybarQueue.js
+++ src/components/playbar/PlaybarQueue.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useCallback } from "react";
 import QueueCard from "../QueueCard";
+import { setCurrentSongIndex, playPause } from "../../reduxStore/playerSlice";
+import { useDispatch } from "react-redux";
 
 function PlaybarQueue({ queue }) {
+  const dispatch = useDispatch();
+
+  const handleTrackClick = useCallback(
+    (trackId) => {
+      const index = queue.findIndex((t) => t.id === trackId);
+      if (index === -1) return;
+
+      dispatch(setCurrentSongIndex(index));
+      dispatch(playPause(true));
+    },
+    [queue, dispatch]
+  );
   return (
     <div className="flex-1 overflow-y-auto hide-scrollbar p-1">
-      {queue.map(track => (
-        <QueueCard key={track.id} track={track} />
+      {queue.map((track) => (
+        <QueueCard key={track.id} track={track} onSelect={handleTrackClick} />
       ))}
     </div>
   );
-};
+}
 
 export default React.memo(PlaybarQueue);
-
